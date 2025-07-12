e9da17696a566cc12dd35bcd9dab2afd2d233a52
Sher110106 <sher.singh.ug23@plaksha.edu.in>
2025-07-12 20:03:02 +0530

    Ownership

---
+++ app/oauth/page.tsx
@@ -0,0 +1,7 @@
+export default function OAuthPage() {
+  return (
+    <div>
+      This page is to verify that this app belongs to sherpartap1996@gmail.com and this is to verify the ownership of the app.
+    </div>
+  );
+}
