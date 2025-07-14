0caee4173d803b6f9dfc9bd726c9c427bc399939
Sher110106 <sher.singh.ug23@plaksha.edu.in>
2025-07-14 11:11:24 +0530

    Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { ThemeProvider } from "next-themes";
 import Link from "next/link";
 import Image from "next/image";
 import "./globals.css";
+import Script from "next/script";
 
 const defaultUrl = 'https://sher-sable.vercel.app'
 
@@ -29,6 +30,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={geistSans.className} suppressHydrationWarning>
+      <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-XYGMYVJ43M"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XYGMYVJ43M');
+          `}
+        </Script>
+      </head>
       <body className="bg-background text-foreground">
         <ThemeProvider
           attribute="class"
