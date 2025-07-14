723032b6f4643591568b2bce0b03df004c8612e6
Sher110106 <sher.singh.ug23@plaksha.edu.in>
2025-07-14 11:25:31 +0530

    Updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,21 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={geistSans.className} suppressHydrationWarning>
-      <head>
-        {/* Google tag (gtag.js) */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-XYGMYVJ43M"
-          strategy="afterInteractive"
-        />
-        <Script id="gtag-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-XYGMYVJ43M');
-          `}
-        </Script>
-      </head>
+      {/* Removed <head> section with Google Analytics scripts. The <head> is now managed by app/head.tsx. */}
       <body className="bg-background text-foreground">
         <ThemeProvider
           attribute="class"
